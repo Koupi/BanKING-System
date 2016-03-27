9b383b1fb0fc64e294af7b9c524c29de63694d88
larandaA <larandaansil@gmail.com>
2016-03-27 19:42:54 +0300

    Implementation folder created. Xcode project created. Empty .c files created.

---
+++ Implementation/BanKING-System/BanKING-System/main.c
@@ -0,0 +1,35 @@
+//
+//  main.c
+//  BanKING-System
+//
+//  Created by Admin on 27.03.16.
+//  Copyright © 2016 Yawning Turtles. All rights reserved.
+//
+
+#include <stdio.h>
+
+void user_login();
+void operator_menu();
+void administrator_menu();
+
+int main(int argc, const char * argv[]) {
+    
+    user_login();
+    
+    return 0;
+}
+
+void user_login()
+{
+    
+}
+
+void operator_menu()
+{
+    
+}
+
+void administrator_menu()
+{
+    
+}
---
+++ Implementation/BanKING-System/BanKING-System/system.h
@@ -0,0 +1,14 @@
+//
+//  system.h
+//  BanKING-System
+//
+//  Created by Admin on 27.03.16.
+//  Copyright © 2016 Yawning Turtles. All rights reserved.
+//
+
+#ifndef system_h
+#define system_h
+
+#include <stdio.h>
+
+#endif /* system_h */
