952ac2fc3f1f5aebe64495af5669a0f3b43b76ef
larandaA <larandaansil@gmail.com>
2016-03-27 19:42:54 +0300

    Implementation folder created. Xcode project created. Empty .c files created.

---
+++ Implementation/BanKING-System/BanKING-System/operator.h
@@ -0,0 +1,14 @@
+//
+//  operator.h
+//  BanKING-System
+//
+//  Created by Admin on 27.03.16.
+//  Copyright © 2016 Yawning Turtles. All rights reserved.
+//
+
+#ifndef operator_h
+#define operator_h
+
+#include <stdio.h>
+
+#endif /* operator_h */
